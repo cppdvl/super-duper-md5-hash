7ef6123544b3a389b33fec77f09bbc1250ba3aab
CPP Devil Julian Guarin <julian.guarin@jayacompany.com>
2020-11-04 00:40:05 -0500

    Kickstart commit

---
+++ gdm5hash.cpp
@@ -0,0 +1,48 @@
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string>
+#include <openssl/md5.h>
+#include <iomanip>
+#include <iostream>
+
+namespace GMD5HASH { //Guarin Implementation of MD5.
+
+    class HashFile:public std::string {
+        unsigned long mFileSize;
+        static unsigned long get_size_by_fd(int fd) {
+            struct stat statbuf;
+            if(fstat(fd, &statbuf) < 0) exit(-1);
+            return statbuf.st_size;
+        }
+        void format_hash(unsigned char* md){
+            for (auto i = 0; i < MD5_DIGEST_LENGTH; ++i){
+                std::stringstream buff;
+                buff << std::setfill('0') << std::setw(2) << std::hex << (unsigned int) md[i];
+                *this += buff.str();
+            }
+        }
+    public:
+        HashFile(std::string pathname){
+            unsigned char result[MD5_DIGEST_LENGTH];
+            int file_descript;
+            char* file_buffer;
+
+            file_descript = open(pathname.c_str(), O_RDONLY);
+            if(file_descript < 0) exit(-1);
+            mFileSize = GMD5HASH::HashFile::get_size_by_fd(file_descript);
+
+            file_buffer = reinterpret_cast<char*>(mmap(0, mFileSize, PROT_READ, MAP_SHARED, file_descript, 0));
+            MD5((unsigned char*) file_buffer, mFileSize, result);
+            munmap(file_buffer, mFileSize);
+            this->format_hash(result);
+
+        }
+        const unsigned long& FileSize(){
+            return mFileSize;
+        }
+    };
+
+
+}
---
+++ gdm5hash.h
@@ -0,0 +1,26 @@
+//
+// Created by root on 11/4/20.
+//
+
+#ifndef GMD5_GDM5HASH_H
+#define GMD5_GDM5HASH_H
+
+#include <string>
+
+namespace GMD5HASH { //Guarin Implementation of MD5.
+
+    class HashFile:public std::string {
+        unsigned long mFileSize;
+        static unsigned long get_size_by_fd(int fd);
+        void format_hash(unsigned char* md);
+    public:
+        HashFile(std::string pathname);
+        const unsigned long& FileSize(){
+            return mFileSize;
+        }
+    };
+
+
+}
+
+#endif //GMD5_GDM5HASH_H
